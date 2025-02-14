ff414762976c3c9bdfd1219d0cb6b8ad34fe0c31
emma <kkes60128angel@gmail.com>
2025-02-15 00:09:00 +0800

    環境建置更新

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
+import 'bootstrap'
+import './assets/all.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
