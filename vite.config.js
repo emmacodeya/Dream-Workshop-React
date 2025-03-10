81cc942873e38fdab2f8e332ee793ae143ca8ce1
nani0917 <nani820917@gmail.com>
2025-03-10 23:57:38 +0800

    0310更新

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-const repoName = 'Dream-Workshop-React';
-
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: mode === 'production' ? `/${repoName}/` : '/', 
-}));
