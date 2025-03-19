d6a00cc38a7c7d085459c9b98f959d5c1318c904
barry <barry91205@gmail.com>
2025-03-20 00:14:56 +0800

    細節更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
