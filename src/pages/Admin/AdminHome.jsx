45df8e1468a685cbd9ef9238e900a03351b7a89e
emma <kkes60128angel@gmail.com>
2025-03-19 23:07:20 +0800

    新增後台頁面

---
+++ src/pages/Admin/AdminHome.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import AdminSidebar from "../../components/AdminSidebar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const AdminHome = () => {
+  const [activeSection, setActiveSection] = useState("accounts");
+
+  return (
+    <div className="container-fluid admin">
+      <div className="row vh-100">
+        <div className="col-2 bg-dark pt-5">
+           <div className="d-flex align-items-center mb-4 ps-3">
+            <img src="https://dream-workshop-api.onrender.com/assets/images/創夢工坊-logo.png" alt="Logo" style={{ width: "40px", height: "40px" }} className="me-2" />
+            <span className="fs-6 text-white ">Dream Workshop</span>
+          </div>
+
+
+          <AdminSidebar setActiveSection={setActiveSection} activeSection={activeSection} />
+        </div>
+
+        <div className="col-10 py-5">
+          <div className="container">
+            <div className={`content-section text-white ${activeSection === "accounts" ? "d-block" : "d-none"}`}>
+              <h2>會員管理</h2>
+            </div>
+            <div className={`content-section text-white ${activeSection === "startups" ? "d-block" : "d-none"}`}>
+              <h2>創業項目管理</h2>
+            </div>
+            <div className={`content-section text-white ${activeSection === "investors" ? "d-block" : "d-none"}`}>
+              <h2>投資人管理</h2>
+            </div>
+            <div className={`content-section text-white ${activeSection === "activities" ? "d-block" : "d-none"}`}>
+              <h2>活動管理</h2>
+            </div>
+            <div className={`content-section text-white ${activeSection === "pricingPlan" ? "d-block" : "d-none"}`}>
+              <h2>點數管理</h2>
+            </div>
+            <div className={`content-section text-white ${activeSection === "transactions" ? "d-block" : "d-none"}`}>
+              <h2>交易管理</h2>
+            </div>
+            <div className={`content-section text-white ${activeSection === "articles" ? "d-block" : "d-none"}`}>
+              <h2>文章管理</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminHome;
